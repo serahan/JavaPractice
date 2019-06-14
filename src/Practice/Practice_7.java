5fcba8058797b5afa94b9d4043ecb4b944c38fce
은지 정 <seyrahan22@gmail.com>
2019-06-14 17:02:16 +0900

    practice7 마침

--- src/Practice/Practice_7.java
+++ src/Practice/Practice_7.java
@@ -1,8 +1,28 @@
 package Practice;
 
 public class Practice_7 {
-	public void practice7()
-	{
+	public void practice7() {
+		int[][] arr = { { 90, 80, 80 }, 
+						{ 85, 85, 95 }, 
+						{ 95, 70, 75 }, 
+						{ 80, 80, 90 }, 
+						{ 90, 75, 80 } };
+
+		int sum = 0;
+		double avg = 0.0;
 		
+		for (int i = 0; i < arr.length; i++) 
+		{
+			for (int j = 0; j < 3; j++) 
+			{
+				sum += arr[i][j];
+			}
+			avg = (double) sum / 3;
+
+			System.out.println("ID = " + i + ": ÇÕ°è = " + sum + ", Æò±Õ = " + Math.round(avg*100)/100.0);
+			
+			sum=0;
+			avg=0.0;
+		}
 	}
 }
