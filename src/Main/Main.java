66a9ad6e18d4a29e5f6c22227ac5dabfa550db22
은지 정 <seyrahan22@gmail.com>
2019-06-14 17:26:58 +0900

    practice10 마침

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,13 +8,14 @@ import Practice.Practice_5;
 import Practice.Practice_6;
 import Practice.Practice_7;
 import Practice.Practice_8;
+import Practice.Practice_9;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Practice_8 pra8 = new Practice_8();
+		Practice_9 pra9 = new Practice_9();
 		
-		pra8.practice8();
+		pra9.practice9();
 	}
 }
