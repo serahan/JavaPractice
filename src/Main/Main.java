0a27067669f86f8d1f3f1659926cbf10c07167e9
은지 정 <seyrahan22@gmail.com>
2019-06-17 12:14:32 +0900

    practice10 마침

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,5 @@
 package Main;
 
-<<<<<<< HEAD
 import Practice.Practice_01;
 import Practice.Practice_02;
 import Practice.Practice_03;
@@ -11,28 +10,13 @@ import Practice.Practice_07;
 import Practice.Practice_08;
 import Practice.Practice_09;
 import Practice.Practice_10;
-=======
-import Practice.Practice_1;
-import Practice.Practice_2;
-import Practice.Practice_3;
-import Practice.Practice_4;
-import Practice.Practice_5;
-import Practice.Practice_6;
-import Practice.Practice_7;
-import Practice.Practice_8;
-import Practice.Practice_9;
->>>>>>> 66a9ad6e18d4a29e5f6c22227ac5dabfa550db22
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		Practice_09 pra9 = new Practice_09();
-=======
-		Practice_9 pra9 = new Practice_9();
->>>>>>> 66a9ad6e18d4a29e5f6c22227ac5dabfa550db22
+		Practice_10 pra10 = new Practice_10();
 		
-		pra9.practice9();
+		pra10.practice10();
 	}
 }
--- src/Practice/Practice_10.java
+++ src/Practice/Practice_10.java
@@ -1,5 +1,49 @@
 package Practice;
 
-public class Practice_10 {
+import java.util.Scanner;
 
+public class Practice_10 
+{
+	public void practice10() 
+	{
+		int[] arr1 = { 80, 90, 95, 75, 70 };
+		int[] arr2 = { 90, 85, 85, 85, 80 };
+		char alp = 'A';
+		int sum = 0;
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print((char)alp + " : ");
+		for(int i=0;i<arr1.length;i++)
+		{
+			System.out.print(arr1[i]+ " ");
+		}
+		System.out.println();
+		MakeSum(arr1, alp);
+		alp++;
+		
+		System.out.println();
+		
+		System.out.print((char)alp + " : ");
+		for(int i=0;i<arr2.length;i++)
+		{
+			System.out.print(arr2[i] + " ");
+		}
+		System.out.println();
+		MakeSum(arr2, alp);
+		
+		scanner.close();
+	}
+
+	public void MakeSum(int[] arr, char alp) 
+	{
+		int sum = 0;
+
+		for (int i = 0; i < arr.length; i++) 
+		{
+			sum += arr[i];
+		}
+		System.out.println("Sum("+alp+") = "+sum);
+	}
 }
+
